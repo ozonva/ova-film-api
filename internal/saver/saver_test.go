12402e36c371890511239d01205c6a2068db2e48
shurupov <bolteg86@ya.ru>
2021-08-26 02:13:15 +0400

    Task-5

--- internal/saver/saver_test.go
+++ internal/saver/saver_test.go
@@ -32,12 +32,12 @@ var _ = Describe("Saver", func() {
 	})
 
 	Describe("Close method", func() {
-		Context("When 4 movies are added", func() {
-			It("should flush all movies", func() {
+		Context("When 3 movies are added", func() {
+			It("should flush all movies 1 time", func() {
 
 				movieRepo.
 					EXPECT().
-					AddMovies(gomock.Any()).
+					AddMovies(gomock.Eq([]movies.Movie{createMovie(1), createMovie(2), createMovie(3)})).
 					Times(1)
 
 				flusher := flusher.NewFlusher(5, movieRepo)
@@ -46,8 +46,31 @@ var _ = Describe("Saver", func() {
 				saver.Save(createMovie(1))
 				saver.Save(createMovie(2))
 				saver.Save(createMovie(3))
-				saver.Save(createMovie(4))
 				saver.Close()
+
+			})
+		})
+		Context("When movies are added several times with delays", func() {
+			It("should flush all movies 4 times", func() {
+
+				movieRepo.
+					EXPECT().
+					AddMovies(gomock.Any()).
+					Times(4)
+
+				flusher := flusher.NewFlusher(5, movieRepo)
+				saver := saver.NewSaver(10, flusher, 1*time.Second)
+
+				timer := time.NewTimer(500 * time.Millisecond)
+				<-timer.C
+
+				for i := 0; i < 4; i++ {
+					var m uint64 = 0
+					m++
+					saver.Save(createMovie(m))
+					timer := time.NewTimer(1 * time.Second)
+					<-timer.C
+				}
 
 			})
 		})
